fd6c57b68abbe8dbfd7fb06821f56f5768d02483
Mohini Mahato <mohinimahatowork@gmail.com>
2023-01-04 17:54:30 +0530

    Delete Javascript directory

--- Javascript/Looping-Patterns/number-pattern/number-triangle-1.js
+++
@@ -1,9 +0,0 @@
-let n = 5;
-let str="";
-for (let i = 1; i<=n; i++){
-    for (let j= 1; j<=i; j++){
-       str=str + j +" "
-    }
-    str+='\n'; //new line
-}
-console.log(str);
--- Javascript/Looping-Patterns/number-pattern/reverse-triangle.js
+++
@@ -1,9 +0,0 @@
-let n = 5;
-let str = "";
-for (let i = 1; i<=n ; i++){
-    for(let j = 1; j<=n-i+1; j++){
-        str+=j;
-    }
-    str+="\n";
-}
-console.log(str)
